3296ea59cbfa05453465c0b1b5e7327b3664246b
Mike McLane <mm@godaddy.com>
2015-04-02 15:44:37 -0700

    o fixed newline missing

--- collectd.bin/install-rabbitmq-types.sh
+++ collectd.bin/install-rabbitmq-types.sh
@@ -3,4 +3,5 @@
 egrep -q '^rabbitmq_memory value:GAUGE:0:U' /usr/share/collectd/types.db
 export UPDATE=$?
 if [ ${UPDATE} -ne 0 ]; then
-  cat /etc/collectd.bin/rabbitmq.types.db.custom >> /usr/share/collectd/types.dbfi
+  cat /etc/collectd.bin/rabbitmq.types.db.custom >> /usr/share/collectd/types.db
+fi
